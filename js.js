18b280454a456ad23ed15e3458852e88b31145a6
jakobmonkey <jakob@brandmonkey.dk>
2022-08-17 09:00:25 +0200

    attempt fix

--- js.js
+++ js.js
@@ -21,7 +21,7 @@ gsap.to('#myclippath', {
    scrollTrigger: {
       // trigger: '.header',
       // markers: true, 
-      start: '500 top',
+      start: '470 top',
       end: '700 700',
       ease: Power0.easeInOut,
       toggleActions: 'play none none reverse'
@@ -30,7 +30,7 @@ gsap.to('#myclippath', {
 
 gsap.to('.header', {
    scrollTrigger: {
-      start: '500 top',
+      start: '470 top',
       end: '700 700',
       // trigger: '.header',
       toggleActions: 'play none none reverse',
